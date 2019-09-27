82642c2c611d228cfcc881f0cb4a8eb1e38e17f3
RigasIliopoulos <55878257+RigasIliopoulos@users.noreply.github.com>
2019-09-27 20:28:39 +0300

    v3

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="el">
+
+<head>
+  <title>Rigas iliopoulos &#x21D2 Web Designer -  Developer</title>
+  <link rel="shortcut icon" type="image/png" href="https://via.placeholder.com/32x32">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="UTF-8"> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<header>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">WebSiteName</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
+    </div>
+  </nav>
+</header>
+<body>
+hello world
+
+</body>
+</html>
